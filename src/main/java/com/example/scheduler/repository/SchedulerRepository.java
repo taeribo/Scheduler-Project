f122933d0f64a88ac81f00e3fae0412876d389ec
taeheon <psw4497@gamil.com>
2026-06-30 20:06:59 +0900

    SchedulerRepository.java 작성자명 기준 내림 차순 구현

--- src/main/java/com/example/scheduler/repository/SchedulerRepository.java
+++ src/main/java/com/example/scheduler/repository/SchedulerRepository.java
@@ -3,5 +3,16 @@ package com.example.scheduler.repository;
 import com.example.scheduler.entity.Scheduler;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface SchedulerRepository extends JpaRepository<Scheduler, Long> {
+
+    List<Scheduler> findALlByAuthorOrderByModifiedAtDesc(String author);
+
+    List<Scheduler> findAllByOrderByModifiedAtDesc();
 }
+
+// ai로 구현한 메서드 규칙 작성자명 기준으로 일정 조회
+// findAllBY 전체 찾기
+// Author author 필드를 기준으로
+// OrderByModifiedAtDesc modifiedAt 기준으로 내림차순
