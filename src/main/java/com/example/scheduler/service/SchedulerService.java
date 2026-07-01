3d8b2353bfb9da99e6316357ac3d1f78ea81f4fd
taeheon <psw4497@gamil.com>
2026-07-01 11:28:12 +0900

    SchedulerService.java CRUD 로직 및 비밀번호 확인 구현

--- src/main/java/com/example/scheduler/service/SchedulerService.java
+++ src/main/java/com/example/scheduler/service/SchedulerService.java
@@ -5,20 +5,28 @@ import com.example.scheduler.dto.ScheduleResponseDto;
 import com.example.scheduler.entity.Scheduler;
 import com.example.scheduler.repository.SchedulerRepository;
 
+
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
 
+import java.util.ArrayList;
+import java.util.List;
 @Service
+
+
 
 
 public class SchedulerService {
     private final SchedulerRepository schedulerRepository;
 
-    public SchedulerService(SchedulerRepository schedulerRepository){
+    public SchedulerService(SchedulerRepository schedulerRepository) {
         this.schedulerRepository = schedulerRepository;
     }
+
     @Transactional
-    public ScheduleResponseDto create(ScheduleRequestDto requestDto){
+    public ScheduleResponseDto create(ScheduleRequestDto requestDto) {
         Scheduler scheduler = new Scheduler(
                 requestDto.getTitle(),
                 requestDto.getContent(),
@@ -27,7 +35,7 @@ public class SchedulerService {
         );
 
 
-        Scheduler savedScheduler =schedulerRepository.save(scheduler);
+        Scheduler savedScheduler = schedulerRepository.save(scheduler);
 
         ScheduleResponseDto scheduleResponseDto = new ScheduleResponseDto(
                 savedScheduler.getId(),
@@ -39,5 +47,87 @@ public class SchedulerService {
         );
 
         return scheduleResponseDto;
+    }
+
+
+    @Transactional(readOnly = true)
+    public List<ScheduleResponseDto> findAll() {
+
+        List<ScheduleResponseDto> scheduleResponseDtoList = new ArrayList<>();
+
+        List<Scheduler> schedulerList = schedulerRepository.findAll();
+        for (Scheduler scheduler : schedulerList) {
+            ScheduleResponseDto scheduleResponseDto = new ScheduleResponseDto(
+                    scheduler.getId(),
+                    scheduler.getTitle(),
+                    scheduler.getContent(),
+                    scheduler.getAuthor(),
+                    scheduler.getCreatedAt(),
+                    scheduler.getModifiedAt()
+            );
+
+            scheduleResponseDtoList.add(scheduleResponseDto);
+        }
+
+        return scheduleResponseDtoList;
+    }
+
+
+    @Transactional(readOnly = true)
+    public ScheduleResponseDto findOne(Long id) {
+        Scheduler scheduler = getOrThrow(id);
+
+        ScheduleResponseDto scheduleResponseDto = new ScheduleResponseDto(
+                scheduler.getId(),
+                scheduler.getTitle(),
+                scheduler.getContent(),
+                scheduler.getAuthor(),
+                scheduler.getCreatedAt(),
+                scheduler.getModifiedAt()
+        );
+
+        return scheduleResponseDto;
+    }
+
+
+    @Transactional
+    public ScheduleResponseDto updates(Long id, ScheduleRequestDto requestDto) {
+        Scheduler scheduler = getOrThrow(id);
+        if (!scheduler.getPassword().equals(requestDto.getPassword())) {
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN, "비밀번호가 일치하지 않습니다.");
+        }
+
+        scheduler.updateSchedule(
+                requestDto.getTitle(),
+                requestDto.getAuthor()
+        );
+
+        Scheduler updatedScheduler = schedulerRepository.save(scheduler);
+
+        ScheduleResponseDto scheduleResponseDto = new ScheduleResponseDto(
+
+                updatedScheduler.getId(),
+                updatedScheduler.getTitle(),
+                updatedScheduler.getContent(),
+                updatedScheduler.getAuthor(),
+                updatedScheduler.getCreatedAt(),
+                updatedScheduler.getModifiedAt()
+        );
+
+
+        return scheduleResponseDto;
+    }
+
+
+    @Transactional
+    public void delete(Long id) {
+        Scheduler scheduler = getOrThrow(id);
+        schedulerRepository.delete(scheduler);
+    }
+
+    private Scheduler getOrThrow(Long id) {
+        return schedulerRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(
+                        HttpStatus.NOT_FOUND, "일정을 찾을 수 없어요: " + id));
     }
 }
